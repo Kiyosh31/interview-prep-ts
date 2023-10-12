1dbdc56f1244f2191dc568f68cdf6a9813e75ba9
Kiyoshi <david_5043@hotmail.com>
2023-10-12 12:47:28 -0600

    initial setup

---
+++ src/find_sum/find_sum.ts
@@ -0,0 +1,11 @@
+/**
+ * Problem Statement
+In this problem, you have to implement the findSum(arr, value) function, 
+which takes an array arr, a number and value as input and returns an array of two numbers that add up to value.
+ * 
+ */
+
+function findSum(arr: number[], value: number): number {}
+
+const res: number = findSum([1, 21, 3, 14, 5, 60, 7, 6], 81);
+console.log(res);
