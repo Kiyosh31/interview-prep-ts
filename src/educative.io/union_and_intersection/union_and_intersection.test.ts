d22d1612ec9c5d6b2ae80d57c6010b9929aff3ab
Kiyoshi <david_5043@hotmail.com>
2023-10-13 10:23:25 -0600

    added new challenge

--- src/educative.io/union_and_intersection/union_and_intersection.test.ts
+++ src/educative.io/union_and_intersection/union_and_intersection.test.ts
@@ -7,28 +7,51 @@ let list2: SinglyLinkedList<number>;
 beforeEach(() => {
   list1 = new SinglyLinkedList<number>();
   list2 = new SinglyLinkedList<number>();
-
-  list1.insertAtTail(10);
-  list1.insertAtTail(20);
-  list1.insertAtTail(80);
-  list1.insertAtTail(60);
-
-  list2.insertAtTail(15);
-  list2.insertAtTail(20);
-  list2.insertAtTail(30);
-  list2.insertAtTail(60);
-  list2.insertAtTail(45);
 });
 
 describe("Union", () => {
-  it("should joined two linkedlists", () => {
+  it("10 -> 20 -> 80 -> 60 -> 15 -> 30 -> 45", () => {
+    list1.insertAtTail(10);
+    list1.insertAtTail(20);
+    list1.insertAtTail(80);
+    list1.insertAtTail(60);
+
+    list2.insertAtTail(15);
+    list2.insertAtTail(20);
+    list2.insertAtTail(30);
+    list2.insertAtTail(60);
+    list2.insertAtTail(45);
+
     const result = union(list1, list2);
     expect(result.printList()).toStrictEqual([10, 20, 80, 60, 15, 30, 45]);
+  });
+
+  it("12 -> 2-> 43 -> 0 -> 41", () => {
+    list1.insertAtTail(12);
+    list1.insertAtTail(2);
+    list1.insertAtTail(43);
+
+    list2.insertAtTail(0);
+    list2.insertAtTail(41);
+
+    const result = union(list1, list2);
+    expect(result.printList()).toStrictEqual([12, 2, 43, 0, 41]);
   });
 });
 
 describe("Intersection", () => {
-  it("should intersect 2 linkedlist", () => {
+  it("20 -> 60", () => {
+    list1.insertAtTail(10);
+    list1.insertAtTail(20);
+    list1.insertAtTail(80);
+    list1.insertAtTail(60);
+
+    list2.insertAtTail(15);
+    list2.insertAtTail(20);
+    list2.insertAtTail(30);
+    list2.insertAtTail(60);
+    list2.insertAtTail(45);
+
     const result = intersection(list1, list2);
     expect(result.printList()).toStrictEqual([20, 60]);
   });
